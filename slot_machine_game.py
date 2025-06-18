7b54235d3e704bc79e0193490df27eaf29d064a8
sheminith <10554sheminith@gmail.com>
2025-06-18 18:42:19 +0530

    Get slot machine spin function completed

--- slot_machine_game.py
+++ slot_machine_game.py
@@ -1,6 +1,40 @@
-MAX_LINES = 4
+import random
+
+MAX_LINES = 3
 MIN_BET = 1
 MAX_BET = 100
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A" : 2,
+    "B" : 4,
+    "C" : 6,
+    "D" : 8
+}
+
+# slot machine spin function
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+
+    # creating a single symbols list and append all the values in symbol_count variable to it
+    for symbol, symbol_count in symbols:
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    # adding values to grid
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+        columns.append(column)
+
+    return columns
 
 # Deposit money function
 def deposit():
